4b21c91864d60e20c0bcdd3bb1813699c82bb172
Helmut Januschka <helmut@januschka.com>
2008-04-07 19:24:19 +0000

    SVN header
    git-svn-id: https://bartlby.svn.sourceforge.net/svnroot/bartlby@18 bb3be5a3-aa48-0410-9352-e9dca9ba2c39

--- bartlby-php/bartlby.c
+++ bartlby-php/bartlby.c
@@ -1,21 +1,23 @@
+/* $Id$ */
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2005-2008 Helmut Januschka - All Rights Reserved
+ *   Contact: <helmut@januschka.com>, <contact@bartlby.org>
+ *
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, Inc., 675 Mass Ave, Cambridge MA 02139,
+ *   USA; either version 2 of the License, or (at your option) any later
+ *   version; incorporated herein by reference.
+ *
+ *   visit: www.bartlby.org for support
+ * ----------------------------------------------------------------------- */
 /*
-  +----------------------------------------------------------------------+
-  | PHP Version 4                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2003 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 2.02 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available at through the world-wide-web at                           |
-  | http://ww w.php.net/license/2_02.txt.                                 |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-
-  $Id: bartlby.c,v 1.60 2008/03/14 01:11:19 hjanuschka Exp $ 
+$Revision$
+$HeadURL$
+$Date$
+$Author$ 
 */
 
 #ifdef HAVE_CONFIG_H
