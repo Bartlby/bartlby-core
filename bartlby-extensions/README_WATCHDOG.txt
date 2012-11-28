a38d23e21e97da34a292e5a6700ea491f9036551
Helmut Januschka <helmut@januschka.com>
2012-11-28 15:55:15 +0100

    split into repos

--- bartlby-extensions/README_WATCHDOG.txt
+++
@@ -1,2 +0,0 @@
-you have your /dev/watchdog device up and running
-bartlby will then, when extension is loaded tick the device
--- bartlby-ui/add_worker.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "layout.class.php";
-include "config.php";
-include "bartlby-ui.class.php";
-$btl=new BartlbyUi($Bartlby_CONF);
-header("Location: modify_worker.php?new=true");
