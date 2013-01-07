babc7c45dcad8e4a931887fd6a95db3e2e2bd84a
Helmut Januschka <helmut@januschka.com>
2013-01-07 07:50:50 +0100

    header file for dead marker in groups

--- include/bartlby.h
+++ include/bartlby.h
@@ -336,6 +336,9 @@ struct servicegroup {
 	int servicegroup_active;
 	char servicegroup_members[1024];
 	
+	int servicegroup_dead;
+	struct service * dead_marker;
+	
 };
 
 struct servergroup {
@@ -344,7 +347,8 @@ struct servergroup {
 	int servergroup_notify;
 	int servergroup_active;
 	char servergroup_members[1024];
-	
+	int servergroup_dead;
+	struct service * dead_marker;
 };
 
 
