80b515000799a37ebcf9f44d235f7f2b32dafdd1
Helmut Januschka <helmut@januschka.com>
2013-01-16 10:13:11 +0100

    module version bump

--- include/bartlby.h
+++ include/bartlby.h
@@ -84,7 +84,7 @@
 #define PROGNAME "bartlby"
 #define REL_NAME "fusion"
 //#define VERSION  "1.4.0"
-#define EXPECTCORE 1400001 //Module V Check's
+#define EXPECTCORE 1400002 //Module V Check's
 
 #define MAX_CCACHE 1024
 #define MAX_CCACHE_KEY 1024
