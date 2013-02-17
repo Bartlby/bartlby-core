9e6f1ac6541adb77ab303a85887f95648305fb56
Helmut Januschka <helmut@januschka.com>
2013-02-17 13:30:24 +0100

    set BASH as default shell instead of sh->dash

--- trigger/push.sh
+++ trigger/push.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #exit;
 ###############
 # Smstools compatible sms trigger for bartlby
--- trigger/sms.sh
+++ trigger/sms.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############
 # Smstools compatible sms trigger for bartlby
 # <hjanuschka> 2008
