2d5db5fd11655432c62415bd3b683152a99f4d98
Helmut Januschka <helmut@januschka.com>
2013-02-22 09:01:35 +0100

    * changed DB Fields of ack settings to INT * ack stays now permantent and can be modified via UI
    Signed-off-by: hjanuschka <helmut@januschka.com>

--- src/libs/mysql.c
+++ src/libs/mysql.c
@@ -54,9 +54,9 @@ $Author$
 
 #define SERVER_MAP_SELECTOR "select server_id, server_ip, server_name, server_ico, server_enabled, server_port, server_dead, server_flap_seconds, server_notify, server_ssh_keyfile, server_ssh_passphrase, server_ssh_username, enabled_triggers from servers"
 
-#define SELECTOR "select svc.service_id, svc.service_name, svc.service_state, srv.server_name, srv.server_id, srv.server_port, srv.server_ip, svc.service_plugin, svc.service_args, UNIX_TIMESTAMP(svc.service_last_check), svc.service_interval, svc.service_text, 'a', 'a', 'a','a', svc.service_notify, svc.service_type, svc.service_var, svc.service_passive_timeout,service_active, svc.service_check_timeout, srv.server_ico, svc.service_ack, svc.service_retain, svc.service_snmp_community, svc.service_snmp_objid, svc.service_snmp_version, svc.service_snmp_warning, svc.service_snmp_critical, svc.service_snmp_type, svc.flap_seconds, svc.service_exec_plan, svc.renotify_interval, svc.escalate_divisor, svc.fires_events, svc.enabled_triggers, svc.service_snmp_textmatch, UNIX_TIMESTAMP(svc.service_last_notify_send), UNIX_TIMESTAMP(svc.service_last_state_change),service_retain_current   from services svc, servers srv where svc.server_id=srv.server_id ORDER BY RAND()"
+#define SELECTOR "select svc.service_id, svc.service_name, svc.service_state, srv.server_name, srv.server_id, srv.server_port, srv.server_ip, svc.service_plugin, svc.service_args, UNIX_TIMESTAMP(svc.service_last_check), svc.service_interval, svc.service_text, 'a', 'a', 'a','a', svc.service_notify, svc.service_type, svc.service_var, svc.service_passive_timeout,service_active, svc.service_check_timeout, srv.server_ico, svc.service_ack_enabled, svc.service_retain, svc.service_snmp_community, svc.service_snmp_objid, svc.service_snmp_version, svc.service_snmp_warning, svc.service_snmp_critical, svc.service_snmp_type, svc.flap_seconds, svc.service_exec_plan, svc.renotify_interval, svc.escalate_divisor, svc.fires_events, svc.enabled_triggers, svc.service_snmp_textmatch, UNIX_TIMESTAMP(svc.service_last_notify_send), UNIX_TIMESTAMP(svc.service_last_state_change),service_retain_current, service_ack_current   from services svc, servers srv where svc.server_id=srv.server_id ORDER BY RAND()"
 #define WORKER_SELECTOR "select worker_mail, worker_icq, enabled_services ,notify_levels, worker_active, worker_name, worker_id, password, enabled_triggers, escalation_limit, escalation_minutes, notify_plan from workers"
-#define SERVICE_UPDATE_TEXT "update services set service_last_check=FROM_UNIXTIME(%d), service_text='%s', service_state=%d, service_last_notify_send=FROM_UNIXTIME(%d), service_last_state_change=FROM_UNIXTIME(%d), service_ack=%d, service_retain_current=%d where service_id=%ld"
+#define SERVICE_UPDATE_TEXT "update services set service_last_check=FROM_UNIXTIME(%d), service_text='%s', service_state=%d, service_last_notify_send=FROM_UNIXTIME(%d), service_last_state_change=FROM_UNIXTIME(%d), service_ack_current=%d, service_retain_current=%d where service_id=%ld"
 
 
 
@@ -71,12 +71,12 @@ $Author$
 
 #define DELETE_SERVICE_BY_SERVER "delete from services where server_id=%d"
 
-#define ADD_SERVICE "insert into services(server_id, service_plugin, service_name, service_state,service_text, service_args,service_notify, service_active, service_interval, service_type,service_var,service_passive_timeout,service_check_timeout, service_ack, service_retain, service_snmp_community, service_snmp_objid, service_snmp_version, service_snmp_warning, service_snmp_critical, service_snmp_type, flap_seconds, service_exec_plan,renotify_interval, escalate_divisor, fires_events, enabled_triggers, service_snmp_textmatch) values(%ld,'%s','%s',4, 'Newly created', '%s',%d,%d,%ld,%d,'%s',%ld, %ld, %d, %ld, '%s', '%s', %d, %ld, %ld, %ld, %ld, '%s',%ld,%ld, %ld, '%s', '%s')"
+#define ADD_SERVICE "insert into services(server_id, service_plugin, service_name, service_state,service_text, service_args,service_notify, service_active, service_interval, service_type,service_var,service_passive_timeout,service_check_timeout, service_ack_enabled, service_retain, service_snmp_community, service_snmp_objid, service_snmp_version, service_snmp_warning, service_snmp_critical, service_snmp_type, flap_seconds, service_exec_plan,renotify_interval, escalate_divisor, fires_events, enabled_triggers, service_snmp_textmatch) values(%ld,'%s','%s',4, 'Newly created', '%s',%d,%d,%ld,%d,'%s',%ld, %ld, %d, %ld, '%s', '%s', %d, %ld, %ld, %ld, %ld, '%s',%ld,%ld, %ld, '%s', '%s')"
 #define DELETE_SERVICE "delete from services where service_id=%d"
 #define SERVICE_CHANGE_ID "update services set service_id=%d where service_id=%d"
 
-#define UPDATE_SERVICE "update services set service_type=%d,service_name='%s',server_id=%ld,service_interval = %ld, service_plugin='%s',service_args='%s',service_passive_timeout=%ld, service_var='%s',service_check_timeout=%ld, service_ack='%d', service_retain='%ld', service_snmp_community='%s', service_snmp_objid='%s', service_snmp_version='%d', service_snmp_warning='%ld', service_snmp_critical='%ld', service_snmp_type='%ld', service_notify='%d', service_active='%d', flap_seconds='%ld', service_exec_plan='%s',renotify_interval=%ld, escalate_divisor=%ld, fires_events=%ld, enabled_triggers='%s', service_snmp_textmatch='%s'  where service_id=%ld"
-#define SERVICE_SELECTOR "select svc.service_id, svc.service_name, svc.service_state, srv.server_name, srv.server_id, srv.server_port, srv.server_ip, svc.service_plugin, svc.service_args, UNIX_TIMESTAMP(svc.service_last_check), svc.service_interval, svc.service_text, 'a', 'a','a', 'a', svc.service_notify, svc.service_type, svc.service_var, svc.service_passive_timeout, svc.service_active,svc.service_check_timeout, svc.service_ack, svc.service_retain, svc.service_snmp_community, svc.service_snmp_objid, svc.service_snmp_version, svc.service_snmp_warning, svc.service_snmp_critical, svc.service_snmp_type, srv.server_ico, svc.flap_seconds, svc.service_exec_plan, svc.renotify_interval, svc.escalate_divisor, svc.fires_events, srv.server_ssh_keyfile, srv.server_ssh_passphrase, srv.server_ssh_username, svc.enabled_triggers, srv.enabled_triggers, svc.service_snmp_textmatch, UNIX_TIMESTAMP(svc.service_last_notify_send), UNIX_TIMESTAMP(svc.service_last_state_change)  from services svc, servers srv where svc.server_id=srv.server_id and svc.service_id=%d"
+#define UPDATE_SERVICE "update services set service_type=%d,service_name='%s',server_id=%ld,service_interval = %ld, service_plugin='%s',service_args='%s',service_passive_timeout=%ld, service_var='%s',service_check_timeout=%ld, service_ack_enabled='%d', service_retain='%ld', service_snmp_community='%s', service_snmp_objid='%s', service_snmp_version='%d', service_snmp_warning='%ld', service_snmp_critical='%ld', service_snmp_type='%ld', service_notify='%d', service_active='%d', flap_seconds='%ld', service_exec_plan='%s',renotify_interval=%ld, escalate_divisor=%ld, fires_events=%ld, enabled_triggers='%s', service_snmp_textmatch='%s'  where service_id=%ld"
+#define SERVICE_SELECTOR "select svc.service_id, svc.service_name, svc.service_state, srv.server_name, srv.server_id, srv.server_port, srv.server_ip, svc.service_plugin, svc.service_args, UNIX_TIMESTAMP(svc.service_last_check), svc.service_interval, svc.service_text, 'a', 'a','a', 'a', svc.service_notify, svc.service_type, svc.service_var, svc.service_passive_timeout, svc.service_active,svc.service_check_timeout, svc.service_ack_enabled, svc.service_retain, svc.service_snmp_community, svc.service_snmp_objid, svc.service_snmp_version, svc.service_snmp_warning, svc.service_snmp_critical, svc.service_snmp_type, srv.server_ico, svc.flap_seconds, svc.service_exec_plan, svc.renotify_interval, svc.escalate_divisor, svc.fires_events, srv.server_ssh_keyfile, srv.server_ssh_passphrase, srv.server_ssh_username, svc.enabled_triggers, srv.enabled_triggers, svc.service_snmp_textmatch, UNIX_TIMESTAMP(svc.service_last_notify_send), UNIX_TIMESTAMP(svc.service_last_state_change), service_retain_current, service_ack_current  from services svc, servers srv where svc.server_id=srv.server_id and svc.service_id=%d"
 
 
 
@@ -1089,6 +1089,9 @@ int GetServiceById(int service_id, struct service * svc, char * config) {
       		svc->last_notify_send=atoi(row[42]);
       		svc->last_state_change=atoi(row[43]);
       		
+      		svc->service_retain_current=atoi(row[44]);
+      		svc->service_ack_current=atoi(row[45]);
+      		
       		if(svc->last_notify_send == 0) svc->last_notify_send = time(NULL);
       		if(svc->last_state_change == 0) svc->last_state_change = time(NULL);
       		
@@ -1151,10 +1154,8 @@ int GetServiceById(int service_id, struct service * svc, char * config) {
       		svc->service_active=atoi(row[20]);
       		svc->service_check_timeout=atoi(row[21]);
       		if(row[22] != NULL) {
-      			svc->service_ack = atoi(row[22]);	
-      		} else {
-      			svc->service_ack = ACK_NOT_NEEDED;
-      		}
+      			svc->service_ack_enabled = atoi(row[22]);	
+      		} 
       		svc->service_retain=atoi(row[23]);
       		svc->flap_count=0;
       		
@@ -1189,7 +1190,7 @@ int GetServiceById(int service_id, struct service * svc, char * config) {
       		svc->fires_events=atoi(row[35]);
 				
 					
-	
+					if(svc->service_ack_enabled == 0) svc->service_ack_current=0;
 	
     		svc->is_gone = 0;  		
       		tmprc=0;
@@ -1264,7 +1265,7 @@ int UpdateService(struct service * svc, char *config) {
 	svc->service_passive_timeout,
 	svc->service_var,
 	svc->service_check_timeout,
-	svc->service_ack,
+	svc->service_ack_enabled,
 	svc->service_retain,
 	svc->snmp_info.community,
 	svc->snmp_info.objid,
@@ -1425,7 +1426,7 @@ int AddService(struct service * svc, char *config) {
 	svc->service_var,
 	svc->service_passive_timeout,
 	svc->service_check_timeout,
-	svc->service_ack,
+	svc->service_ack_enabled,
 	svc->service_retain,
 	svc->snmp_info.community,
 	svc->snmp_info.objid,
@@ -1805,7 +1806,7 @@ int doUpdate(struct service * svc, char * config) {
 	service_mysql_safe(svc);
 	
 	
-	asprintf(&sqlupd, SERVICE_UPDATE_TEXT, svc->last_check, svc->new_server_text, svc->current_state, svc->last_notify_send, svc->last_state_change, svc->service_ack,svc->service_retain_current, svc->service_id);
+	asprintf(&sqlupd, SERVICE_UPDATE_TEXT, svc->last_check, svc->new_server_text, svc->current_state, svc->last_notify_send, svc->last_state_change, svc->service_ack_current,svc->service_retain_current, svc->service_id);
 	
 	
 	mysql_query(mysql, sqlupd);
@@ -2074,14 +2075,13 @@ int GetServiceMap(struct service * svcs, char * config) {
       			
       			
       			if(row[23] != NULL) {
-      				svcs[i].service_ack = atoi(row[23]);
-      			} else {
-      				svcs[i].service_ack = ACK_NOT_NEEDED;
+      				svcs[i].service_ack_enabled = atoi(row[23]);
       			}
       			
       			svcs[i].service_retain=atoi(row[24]);
       			svcs[i].service_retain_current=atoi(row[40]);
-      			
+      			svcs[i].service_ack_current=atoi(row[41]);
+      			//DFF
       			svcs[i].flap_count=0;
       			svcs[i].process.pid = 0;
       			svcs[i].process.start_time = 0;
@@ -2146,6 +2146,8 @@ int GetServiceMap(struct service * svcs, char * config) {
       			} else {
       				sprintf(svcs[i].snmp_info.textmatch, "%s", "");	
       			}
+      			
+      			if(svcs[i].service_ack_enabled == 0) svcs[i].service_ack_current=0;
       				
       			//bartlby_replace_svc_in_str(svcs[i].plugin_arguments, &svcs[i], 2048);
       			i++;
