a8f420cdcbdd9f6d348aae93f24f5e6f42de4120
Helmut Januschka <helmut@januschka.com>
2014-11-30 23:36:44 +0100

    intermediate files for traps

--- src/libs/mysql.c
+++ src/libs/mysql.c
@@ -61,6 +61,94 @@ static MYSQL * mysql_conn;
 #define NAME "MYSQL Connector"
 #define DLVERSION  "1.5.1"
 
+
+
+///TRAPS
+#define UPDATE_TRAP "update traps \
+                                    set \
+                                      trap_name='%s', \
+                                      trap_catcher='%s', \
+                                      trap_status_text='%s', \
+                                      trap_status_ok='%s', \
+                                      trap_status_warning='%s', \
+                                      trap_status_critical='%s', \
+                                      trap_service_id=%d, \
+                                      trap_fixed_status=%d, \
+                                      trap_prio=%d, \
+                                      trap_is_final=%d, \
+                                      orch_id=%d \
+                                    where \
+                                      trap_id=%ld"
+
+#define DEL_TRAP "delete from traps where trap_id=%d"
+#define ADD_TRAP "INSERT INTO \
+                                      traps( \
+                                          trap_name, \
+                                          trap_catcher, \
+                                          trap_status_text, \
+                                          trap_status_ok, \
+                                          trap_status_warning, \
+                                          trap_status_critical, \
+                                          trap_service_id, \
+                                          trap_fixed_status, \
+                                          trap_prio, \
+                                          trap_is_final, \
+                                          orch_id \
+                                        ) \
+                                      VALUES( \
+                                          '%s', \
+                                          '%s', \
+                                          '%s', \
+                                          '%s', \
+                                          '%s', \
+                                          '%s', \
+                                          '%d' \
+                                          '%d' \
+                                          '%d' \
+                                          '%d' \
+                                          '%d' \
+                                      )"
+#define TRAP_SEL "select \
+                            trap_id, \
+                            trap_name, \
+                            trap_catcher, \
+                            trap_status_text, \
+                            trap_status_ok, \
+                            trap_status_warning, \
+                            trap_status_critical, \
+                            trap_service_id, \
+                            trap_fixed_status, \
+                            trap_prio, \
+                            trap_is_final, \
+                            orch_id  \
+                          from \
+                            traps %s"
+
+
+
+#define TRAP_SELECTOR "select \
+                            trap_id, \
+                            trap_name, \
+                            trap_catcher, \
+                            trap_status_text, \
+                            trap_status_ok, \
+                            trap_status_warning, \
+                            trap_status_critical, \
+                            trap_service_id, \
+                            trap_fixed_status, \
+                            trap_prio, \
+                            trap_is_final, \
+                            orch_id  \
+                          from \
+                            traps where trap_id=%ld"
+
+                            
+
+
+#define TRAP_CHANGE_ID "update traps set trap_id=%d where trap_id=%d"
+///TRAPS;
+
+
 #define SERVER_MAP_SELECTOR "select server_id, \
                                     server_ip, \
                                     server_name, \
@@ -4035,4 +4123,403 @@ int UpdateServiceGroup(struct servicegroup * svc, char *config) {
 	free(mysql_db);
 	
 	return rtc;	
+}
+
+
+
+
+
+int AddTrap(struct trap * svc, char *config) {
+  
+  MYSQL *mysql;
+  int rtc;
+  
+  char * sqlupd;
+  
+
+
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+  mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+  
+  
+  CHECKED_ASPRINTF(&sqlupd, ADD_TRAP, 
+                            svc->trap_name,
+                            svc->trap_catcher,
+                            svc->trap_status_text,
+                            svc->trap_status_ok,
+                            svc->trap_status_warning,
+                            svc->trap_status_critical,
+                            svc->trap_service_id,
+                            svc->trap_fixed_status,
+                            svc->trap_prio,
+                            svc->trap_is_final,
+                            svc->orch_id
+  );
+  
+  
+
+  mysql_query(mysql, sqlupd);
+    CHK_ERR(mysql,NULL);
+  
+  
+  free(sqlupd);
+  rtc=mysql_insert_id(mysql);
+  mysql_close(mysql);
+    
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  
+  return rtc; 
+} 
+
+
+int DeleteTrap(int trap_id, char * config) {
+  
+  MYSQL *mysql;
+
+  
+  char * sqlupd;
+  
+
+
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+  mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+  
+  
+  CHECKED_ASPRINTF(&sqlupd, DEL_TRAP, trap_id);
+  
+  //Log("dbg", sqlupd);
+  
+  mysql_query(mysql, sqlupd);
+    CHK_ERR(mysql,NULL);
+  
+  
+  free(sqlupd);
+  
+  mysql_close(mysql);
+    
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  
+  return 1;   
+  
+  
+}
+
+int UpdateTrap(struct trap * svc, char *config) {
+
+  MYSQL *mysql;
+  int rtc;
+  
+  char * sqlupd;
+  
+
+
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+  mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+  
+  CHECKED_ASPRINTF(&sqlupd, UPDATE_TRAP,svc->trap_name,
+                            svc->trap_catcher,
+                            svc->trap_status_text,
+                            svc->trap_status_ok,
+                            svc->trap_status_warning,
+                            svc->trap_status_critical,
+                            svc->trap_service_id,
+                            svc->trap_fixed_status,
+                            svc->trap_prio,
+                            svc->trap_is_final,
+                            svc->orch_id,
+                            svc->trap_id);
+  mysql_query(mysql, sqlupd);
+    CHK_ERR(mysql,NULL);
+  
+  
+  free(sqlupd);
+  rtc=1;
+  mysql_close(mysql);
+    
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  
+  return rtc; 
+}
+
+int TrapChangeId(int from, int to, char * config) {
+  MYSQL *mysql;
+  
+  
+  char * sqlupd;
+  
+
+
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+  mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+  
+  
+  CHECKED_ASPRINTF(&sqlupd, TRAP_CHANGE_ID, to, from);
+  
+  
+  
+  mysql_query(mysql, sqlupd);
+    CHK_ERR(mysql,NULL);
+      
+  free(sqlupd);
+    
+  
+  mysql_close(mysql);
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  return to;  
+}
+
+
+int GetTrapById(int trap_id, struct trap * svc, char * config) {
+  
+  int tmprc;
+  MYSQL *mysql;
+  MYSQL_ROW  row;
+  MYSQL_RES  *res;
+  char * sqlupd;
+  
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+  
+  
+  
+  
+
+  
+  
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+        mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+  
+  CHECKED_ASPRINTF(&sqlupd, TRAP_SELECTOR, trap_id);
+  
+  
+  mysql_query(mysql, sqlupd);
+    CHK_ERR(mysql,NULL);
+        res = mysql_store_result(mysql);
+          CHK_ERR(mysql,NULL);
+        
+        
+        if(mysql_num_rows(res) == 1 ) {
+          row=mysql_fetch_row(res);
+          svc->trap_id = atol(row[0]);
+
+          if(row[1] != NULL) {
+            sprintf(svc->trap_name, "%s", row[1]);
+          } else {
+            sprintf(svc->trap_name, "(null)");          
+          }
+          if(row[2] != NULL) {
+            sprintf(svc->trap_catcher, "%s", row[2]);
+          } else {
+            sprintf(svc->trap_catcher, "(null)");          
+          }
+          if(row[3] != NULL) {
+            sprintf(svc->trap_status_text, "%s", row[3]);
+          } else {
+            sprintf(svc->trap_status_text, "(null)");          
+          }
+          if(row[4] != NULL) {
+            sprintf(svc->trap_status_ok, "%s", row[4]);
+          } else {
+            sprintf(svc->trap_status_ok, "(null)");          
+          }
+          if(row[5] != NULL) {
+            sprintf(svc->trap_status_warning, "%s", row[5]);
+          } else {
+            sprintf(svc->trap_status_warning, "(null)");          
+          }
+          if(row[6] != NULL) {
+            sprintf(svc->trap_status_critical, "%s", row[6]);
+          } else {
+            sprintf(svc->trap_status_critical, "(null)");          
+          }
+          svc->trap_service_id = atol(row[7]);
+          svc->trap_fixed_status = atol(row[8]);
+          svc->trap_prio = atol(row[9]);
+          svc->trap_is_final = atol(row[10]);
+          svc->orch_id = atol(row[11]);
+          svc->service_shm_place=-2;
+          tmprc=0;
+        } else {
+          tmprc=-1;
+        }
+  
+  
+  mysql_free_result(res);
+  mysql_close(mysql);
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  free(sqlupd);
+  return tmprc;
+    
+}
+
+int GetTrapMap(struct trap * svcs, char * config, int orch_id) {
+  
+  MYSQL *mysql;
+  MYSQL_ROW  row;
+  MYSQL_RES  *res;
+  
+  
+  char * mysql_host = getConfigValue("mysql_host", config);
+  char * mysql_user = getConfigValue("mysql_user", config);
+  char * mysql_pw = getConfigValue("mysql_pw", config);
+  char * mysql_db = getConfigValue("mysql_db", config);
+  int i=0;
+  
+  char * sql, *where;
+
+
+  set_cfg(config);
+  mysql=mysql_init(NULL);
+    CHK_ERR(mysql,NULL);
+  mysql=mysql_real_connect(mysql, mysql_host, mysql_user, mysql_pw, NULL, 0, NULL, 0);
+    CHK_ERR(mysql,NULL);
+        mysql_select_db(mysql, mysql_db);
+          CHK_ERR(mysql,NULL);
+          
+
+        if(orch_id > 0) {
+          CHECKED_ASPRINTF(&where, " where orch_id=%d", orch_id);
+        } else {
+          CHECKED_ASPRINTF(&where, " ");
+        }
+        CHECKED_ASPRINTF(&sql, TRAP_SEL, where);
+
+        mysql_query(mysql, sql);
+    CHK_ERR(mysql,NULL);
+
+    
+    free(where);
+    free(sql);
+
+
+  res = mysql_store_result(mysql);
+          CHK_ERR(mysql,NULL);
+        
+        
+        if(mysql_num_rows(res) > 0) {
+          
+          while ( (row=mysql_fetch_row(res)) != NULL) {
+              svcs[i].trap_id = atol(row[0]);
+              if(row[1] != NULL) {
+                sprintf(svcs[i].trap_name, "%s", row[1]);
+              } else {
+                sprintf(svcs[i].trap_name, "(null)");          
+              }
+              if(row[2] != NULL) {
+                sprintf(svcs[i].trap_catcher, "%s", row[2]);
+              } else {
+                sprintf(svcs[i].trap_catcher, "(null)");          
+              }
+              if(row[3] != NULL) {
+                sprintf(svcs[i].trap_status_text, "%s", row[3]);
+              } else {
+                sprintf(svcs[i].trap_status_text, "(null)");          
+              }
+              if(row[4] != NULL) {
+                sprintf(svcs[i].trap_status_ok, "%s", row[4]);
+              } else {
+                sprintf(svcs[i].trap_status_ok, "(null)");          
+              }
+              if(row[5] != NULL) {
+                sprintf(svcs[i].trap_status_warning, "%s", row[5]);
+              } else {
+                sprintf(svcs[i].trap_status_warning, "(null)");          
+              }
+              if(row[6] != NULL) {
+                sprintf(svcs[i].trap_status_critical, "%s", row[6]);
+              } else {
+                sprintf(svcs[i].trap_status_critical, "(null)");          
+              }
+              svcs[i].trap_service_id = atol(row[7]);
+              svcs[i].trap_fixed_status = atol(row[8]);
+              svcs[i].trap_prio = atol(row[9]);
+              svcs[i].trap_is_final = atol(row[10]);
+              svcs[i].orch_id = atol(row[11]);
+              svcs[i].service_shm_place=-2;
+
+
+
+            i++;
+          }
+          
+          mysql_free_result(res);
+          mysql_close(mysql);
+          free(mysql_host);
+    free(mysql_user);
+    free(mysql_pw);
+    free(mysql_db);
+          return i;
+        } else { 
+          _log(LH_LIB, B_LOG_INFO, "no Traps found!");  
+        }
+  
+  
+  
+  mysql_free_result(res);
+  mysql_close(mysql);
+  free(mysql_host);
+  free(mysql_user);
+  free(mysql_pw);
+  free(mysql_db);
+  
+  return -1;
+  
+  
 }
